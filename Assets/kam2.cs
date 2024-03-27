6a92f2ae93ec8c822bcee842ae0de8a8d2a706fd
1900134 <1900134@TE-GDA-D-D36G11>
2024-03-27 14:11:29 +0100

    nie lata

--- Assets/kam2.cs
+++ Assets/kam2.cs
@@ -1,18 +1,44 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
-public class kam2 : MonoBehaviour
+public class CCTVCameraController : MonoBehaviour
 {
+    //modyfikator pr�dko�ci obrotu
+    public float turnSpeed = 5f;
+
+    //zakres obrotu kamery w stopniach
+    public float turnAngle = 90;
+
+    //czy kamera kr�ci si� w prawo
+    bool turningRight = !true;
+
+    //obiektyw kamery
+    Transform cameraLens;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Transform cameraPosition = transform.Find("CameraPosition");
+        cameraLens = cameraPosition.Find("Cylinder");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // mdyfikujemy rotacj� obiektu za pomoc� funkcji PingPong, kt�ra generuje warto�ci
+        // oscyluj�ce pomi�dzy 0 a 9, a nast�pnie mno�ymy to przez 10 �eby uzyska� szybszy ruch
+        // i na koniec odejmujemy od warto�ci otrzymanej 45 stopni aby uzyska� ruch w zakresie -45 do 45
+        transform.rotation = Quaternion.Euler(new Vector3(0, Mathf.PingPong(Time.time, 9) * 10 - 45, 0));
+
+        CheckIfPlayerVisible();
     }
+
+    void CheckIfPlayerVisible()
+    {
+        // Debug.DrawRay(cameraLens.position, cameraLens.TransformDirection(Vector3.down)*100, Color.yellow);
+    }
+
 }
+
--- Assets/playercontroller.cs
+++ Assets/playercontroller.cs
@@ -6,7 +6,7 @@ public class PlayerController : MonoBehaviour
 {
     //silnik fizyczny dla obiektu gracza
     Rigidbody rb;
-    //siła skoku
+    //si�a skoku
     public float jumpForce = 5f;
 
     public float moveSpeed = 5f;
@@ -21,42 +21,42 @@ public class PlayerController : MonoBehaviour
     void FixedUpdate()
     {
         //transform.position += new Vector3(1, 0, 0) * Time.deltaTime;
-        //można prościej: Vector3.right
+        //mo�na pro�ciej: Vector3.right
 
-        //zczytaj klawiaturę w osi poziomej:
+        //zczytaj klawiatur� w osi poziomej:
         float horizontalInput = Input.GetAxisRaw("Horizontal");
-        //wyświetl w konsoli stan klawiatury
-        Debug.Log(horizontalInput);
+        //wy�wietl w konsoli stan klawiatury
+        //Debug.Log(horizontalInput);
 
-        //wylicz przesunięcie w osi x
+        //wylicz przesuni�cie w osi x
         Vector3 movement = Vector3.right * horizontalInput;
 
-        //zczytaj z klawiatury oś y
+        //zczytaj z klawiatury o� y
         float verticalInput = Input.GetAxisRaw("Vertical");
 
-        //wylicz przesunięcie w osi y i dodaj do istniejącego przesunięcia w osi x
+        //wylicz przesuni�cie w osi y i dodaj do istniej�cego przesuni�cia w osi x
         movement += Vector3.forward * verticalInput;
 
         //normalizujemy wektor
         movement = movement.normalized;
         //poprawka na czas od ostatniej klatki
         movement *= Time.deltaTime;
-        //pomnóż przez prędkość ruchu
+        //pomn� przez pr�dko�� ruchu
         movement *= moveSpeed;
 
-        //przesuń gracza w osi x
+        //przesu� gracza w osi x
         //transform.position += movement;
 
-        //próbujemy użyc translate zamiast dodawac współrzędne
+        //pr�bujemy u�yc translate zamiast dodawac wsp�rz�dne
         transform.Translate(movement);
 
 
     }
-    //spróbujmy obejść problem z opóźnieniem wejścia poprzez przeniesienie go do update
+    //spr�bujmy obej�� problem z op�nieniem wej�cia poprzez przeniesienie go do update
     void Update()
     {
-        //sprawdz czy nacisnieto spację (skok)
-        //zwraca true jeśli zaczęliśmy naciskać spację w trakcie klatki animacji
+        //sprawdz czy nacisnieto spacj� (skok)
+        //zwraca true je�li zacz�li�my naciska� spacj� w trakcie klatki animacji
         if (Input.GetKeyDown(KeyCode.Space))
         {
             Jump();
@@ -64,12 +64,17 @@ public class PlayerController : MonoBehaviour
     }
     void Jump()
     {
-        //sprawdz czy znajduje się na poziomie 0
+        //sprawdz czy znajduje si� na poziomie 0
         if (transform.position.y <= Mathf.Epsilon)
         {
-            //dodaj siłę skoku
+            //dodaj si�� skoku
             rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
         }
 
     }
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("BOOM");
+    }
 }
+
